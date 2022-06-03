d95ac46e3652054e79691863ed8abfaf991bc953
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-06-03 13:17:36 -0700

    n

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints numbers of base 10
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int n = 0;
+while (n < 10)
+	printf("%d\n", n);
+	n++
+	return (0);
+}
