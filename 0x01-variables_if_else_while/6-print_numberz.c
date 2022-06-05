0af3b68f6018327ba716cfd598897f66f45a1119
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-06-05 10:21:21 -0700

    h

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main  - prints number from 1 to 10
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int num = '0';
+
+for (num ='0'; num < '9'; num++)
+{
+	putchar(num);
+}
+	putchar('\n');
+	return (0);
+
+}
