9d8006890ad8c38079094cc76aea3fdd999c9239
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-06-05 06:58:01 -0700

    b

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,15 +9,16 @@
  */
 int main(void)
 {
-	int n;
+	int n, last;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if ((n % 10) > 5)
-		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
-	else if ((n % 10) == 0)
-		printf("Last digit %d is %d and is 0\n", n, n % 10);
+	last = n % 10;
+	if (last > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, last);
+	else if (last == 0)
+		printf("Last digit of %d is %d and is 0\n", n, last);
 	else
-		printf("Last digit of %d is %d add is less than 6 and not 0 \n", n, n % 10);
+		printf("Last digit of %d is %d add is less than 6 and not 0 \n", n, last);
 	return (0);
 }
