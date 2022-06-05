21a950c3615afd4cf3aa71297e6630f9efdda0bd
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-06-05 14:35:08 -0700

    j

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,13 @@
+#include <stdio>
+/**
+ * main - prints comniantion of two nubers
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int one;
+	int two;
+	int three;
+
+	for (one = two + 1;)
--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,13 +6,16 @@
  */
 int main(void)
 {
-	int n = '0';
+	int n;
 
-	for (n = '0'; n <= '9'; n++)
+	for (n = 0; n <= 9; n++)
 	{
-		putchar(n);
+		putchar(n + '0');
+		if (n < 9);
+		{
 		putchar(',');
 		putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
