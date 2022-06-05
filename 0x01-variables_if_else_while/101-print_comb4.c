d08db85030b2123483d7a7655e96ee187aaf3d03
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-06-05 15:23:59 -0700

    g

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,14 +1,32 @@
-#include <stdio>
+#include <stdio.h>
 /**
- * main - prints comniantion of two nubers
+ * main - print combinations of any 2 numbers
  *
  * Return: always 0 (success)
  */
 int main(void)
 {
-	int one;
-	int two;
-	int three;
+	int num1;
+	int num2;
+	int num3;
 
-	for (one = two + 1:wq
-;)
+	for (num1 = 0; num1 <= 9; num1++)
+	{
+	for (num2 = num1 + 1; num2 <= 9; num2++)
+	{
+		for (num3 = num2 + 1; num3 <=9; num3++)
+		{
+		putchar(num1 + '0');
+		putchar(num2 + '0');
+		putchar(num3 + '0');
+		if (num1 < 8)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
