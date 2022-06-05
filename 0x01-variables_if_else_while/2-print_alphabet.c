92b4b434433e564ec2c34935bb7b14053197fe1c
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-06-05 08:04:33 -0700

    h

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	char n;
+	n = 'a';
+	while (n >= 'a', n <= 'z' ) 
+	{
+		n++;
+	putchar(n);
+	}
+	return (0);
+}
