41a20bcf5cf06c7e2053be0f03371d412588c195
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-06-07 12:25:43 -0700

    f

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _islower prints the lower case characters
+ *
+ * Return: 1  when lower case and 0 othewise
+ */
+int _islower(int c)
+{
+	if (c = 'a' || c <= 'z');
+	return (1);
+	if (c = 'A'|| c <= 'Z');
+	return (0);
+}
