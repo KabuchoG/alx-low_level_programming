9265be72f8a2d35e64d42b229479b3fcdcf3af02
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-06-07 10:04:45 -0700

    g

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,18 @@
 #include "main.h"
 /**
- *
  * main- prints putchar and a new line
- * Reeturn: always 0 (success)
+ *
+ * Return: always 0 (success)
  */
 int main(void)
 {
 	char n[9] = "_putchar";
 	int i = 0;
-	
+
 	for (i = 0; i < 8; i++)
 	{
 	_putchar(n[i]);
 	}
 	_putchar('\n');
-
 	return (0);
 }
