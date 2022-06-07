66e192094c48d6a8c21a777c3273408fa9a0d444
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-06-07 14:14:56 -0700

    fh

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * abs : determines the absolute value of a number
+ *
+ * Return: returns a value int
+ */
+int _abs(int a)
+{
+	int n;
+	int absolutval;
+	int begin = 0;
+
+	if (n > 0)
+	{
+	absolutval = (begin + n);
+	}
+	else if (n < 0)
+	{
+	absolutval = (begin - n);
+	}
+	else
+	{
+	absolutval = 0;
+	}
+}
