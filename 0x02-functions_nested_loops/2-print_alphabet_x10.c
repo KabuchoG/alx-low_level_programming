97d873b147c3bd8c045f0d058479facd4c687c5f
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-06-07 11:00:52 -0700

    djf

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,14 @@
 #include "main.h"
-/** print_alphabet_x10 - prints alphabets ten times
+/** 
+ * print_alphabet_x10 - prints alphabets ten times
  *
  * Return: void
  */
 void print_alphabet_x10(void)
 {
 	int i, j = 'a';
-	
-	for (i = j; i <= 10; i++)
+
+	for (i = 0; i <= 10; i++)
 {
 	for (j = 'a'; j <= 'z'; j++)
 	{
