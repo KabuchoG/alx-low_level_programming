16a15420642ea7966f43bff93de3d1fc7369fe75
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-06-07 13:35:40 -0700

    jhlf

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sign - prints sign depending on the number
+ * @n: the variable number
+ *
+ * Return: 1 or 0 or -1
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+	return (1);
+	_putchar('+');
+	}
+	else if (n = 0)
+	{
+	return (0);
+	_putchar('0');
+	}
+	else
+	{
+	_putchar('-');
+	}
+	return (-1);
+}
