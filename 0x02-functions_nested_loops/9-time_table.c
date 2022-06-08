26dfecc9c8c38a36a0ef3a35485f13ad4e63f869
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-06-08 06:30:54 -0700

    ff

---
+++ 0x02-functions_nested_loops/9-time_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+ * times_table - print the 9 times table
+ *
+ * Description: prints times table
+ *
+ * Return: void
+ */
+void time_table(void)
+{
+int a, b, prod, tens, ones;
+
+for (a = 0; a <= 9; a++)
+{
+for (b = 0; b <= 9; b++)
+{
+prod = a * b;
+tens = prod / 10;
+ones = prod % 10;
+if (b == 0)
+{
+_putchar('0');
+}
+else if (prod < 10)
+{
+_putchar(',');
+_putcahr(' ');
+_putchar(' ');
+_putchar(ones + '0');
+}
+else
+{
+_putchar(',');
+_putcahr(' ');
+_putchar(tens + '0');
+_putchar(ones + '0');
+}
+}
+_putchar('\n');
+}
+}
