d35f59812591a217ab8b5019252b7e3d9d7f843b
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-06-08 06:04:16 -0700

    gg

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,8 +10,8 @@ void jack_bauer(void)
 {
 	int hours_tens, hours_ones, minutes_ones, minutes_tens, hours_max;
 
-		hours_max = 58;
-	hours_tens + '0';
+	hours_max = 58;
+	hours_tens = '0';
 	while (hours_tens < '3')
 	{
 	if (hours_tens == '2')
