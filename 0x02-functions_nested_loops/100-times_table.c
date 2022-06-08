aaea96167044221f5c8269601b7f147b077be777
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-06-08 06:48:46 -0700

    kf

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,43 +9,42 @@
  */
 void print_times_table(int n)
 {
-	int i, j, prod;
-
-	if (n >= 0 && n < 15)
-	{
-	for (i = 0; i <= n; i++)
-	{ 
-	for (j = 0; j <= n; j++)
-	{
-	prod = i * j;
-	if (j == 0)
-	_putchar('0');
-	else if (prod < 10)
-	{
-	_putchar(',');
-	_putchar(' ');
-	_putchar(' ');
-	_putchar(' ');
-	_putchar(prod % 10 + '0');
-	}
-	else if (prod >= 10 && prod < 100)
-	{
-	_putchar(',');
-	_putchar(' ');
-	_putchar(' ');
-	_putchar((prod / 10) % 10 + '0');
-	_putchar(prod % 10 + '0');
-	}
-	else if (prod > 99 && prod < 1000)
-	{
-	_putchar(',');
-	_putchar(' ');
-	_putchar(prod / 100 + '0');
-	_putchar((prod / 10) % 10 + '0');
-	_putchar(prod % 10 + '0');
-	}
-	}
-	_putchar('\n');
-	}
-	}
+int i, j, prod;
+if (n >= 0 && n < 15)
+{
+for (i = 0; i <= n; i++)
+{ 
+for (j = 0; j <= n; j++)
+{
+prod = i * j;
+if (j == 0)
+_putchar('0');
+else if (prod < 10)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(' ');
+_putchar(prod % 10 + '0');
+}
+else if (prod >= 10 && prod < 100)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar((prod / 10) % 10 + '0');
+_putchar(prod % 10 + '0');
+}
+else if (prod > 99 && prod < 1000)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(prod / 100 + '0');
+_putchar((prod / 10) % 10 + '0');
+_putchar(prod % 10 + '0');
+}
+}
+_putchar('\n');
+}
+}
 }
