a5aee552e8d1367dae6c06b89216047cedbf670e
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-06-09 13:46:36 -0700

    me

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,25 +6,25 @@
  */
 int main(void)
 {
-	int i;
+int i;
 
-	for (i = 0; i <= 100; i++)
-	{
-	if (i % 3 == 0)
-	{
-	printf("Fizz ");
-	}
-	if (i % 5 == 0)
-	{
-	printf("Buzz ");
-	}
-	if ((i % 3 == 0) && (i % 5 == 0))
-	{
-	printf("FizzBuzz ");
-	}
-	if (!((i % 3 == 0 && i % 5 == 0) || (i % 3 == 0) || (i % 5 == 0)))
-	printf("%d ", i);
-	}
-	printf("\n");
-	return (0);
+for (i = 0; i <= 100; i++)
+{
+if (i % 3 == 0)
+{
+printf("Fizz ");
+}
+else if (i % 5 == 0)
+{
+printf("Buzz ");
+}
+else if ((i % 3 == 0) && (i % 5 == 0))
+{
+printf("FizzBuzz ");
+}
+else
+printf("%d ", i);
+}
+printf("\n");
+return (0);
 }
