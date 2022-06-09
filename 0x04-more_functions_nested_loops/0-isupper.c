a2a06e8ec658c4449edae61a0c5942c9ad03c68e
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-06-09 09:03:24 -0700

    new

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * isupper- dtermines if a letter is upper case
+ * @c: the character
+ *Return: 1 if uppercase and 0 if lowercase
+ */
+int _isupper(int c)
+{
+	if ((c = 'A') && c <= 'Z')
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
