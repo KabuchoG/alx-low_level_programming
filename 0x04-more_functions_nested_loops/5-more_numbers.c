a79da3ceb1d9c041c36d8d188a464a86a77afd0d
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-06-09 14:29:39 -0700

    new

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
  * more_numbers: prnts numbers 1 to 14
- * Description: iterates 10 times
+ *
  * Return: void
  */
 void more_numbers(void)
 {
 	int i, j;
 
-	for (i = 1; i <= 10; i ++)
+	for (i = 1; i <= 10; i++)
 	{
 	for (j = 0; j < 15; j++)
 	{
