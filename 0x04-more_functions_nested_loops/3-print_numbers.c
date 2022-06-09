8cf8d30d9fe599a757f656746a13b7ceab5a0e21
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-06-09 09:54:32 -0700

    ne commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - prints nu,bers from 0 to 9
+ * Description: void means it is returning nothing
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+	_putchar(num);
+	}
+	_putchar('\n');
+	return (void);
+}
