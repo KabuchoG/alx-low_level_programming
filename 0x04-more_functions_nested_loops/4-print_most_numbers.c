b4e1af0dff1cd3d462df61f39221fa565ac6541e
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-06-09 11:45:48 -0700

    new

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,11 +6,11 @@
  */
 void print_most_numbers(void)
 {
-	int num;
+	char num;
 
-	for (num = 0; num < 10; num++)
+	for (num = '0'; num <= '9' ; num++)
 	{
-	if (num != 2 || num != 4)
+	if (num != ''2' || num != '4')
 	{
 	_putchar(num);
 	}
