caa5626263e2c71b8b8841760a7889237bf1c538
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-06-14 11:44:38 -0700

    new

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - prints an array of n size
+ * @n the number of elements in the array
+ * @a the array name 
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	for (; a[] <= n; a[]++)
+	{
+	printf("%d, ", a[]);
+	}
+}
