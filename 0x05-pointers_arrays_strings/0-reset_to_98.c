0070db5056d5921ff0db3de3dc0b4b41ebfe16fa
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-06-14 06:48:47 -0700

    new

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
  * reset_to_98 -resets a pointed value to 98
- * @*n: the variable to reset a value
+ * @n: the variable to reset a value
  * Retun: void
  */
 void reset_to_98(int *n)
 {
-int p;
+int p = 50
 
 n = &p;
 *n = 98;
