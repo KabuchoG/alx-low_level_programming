c4c7122fbedd735123a3c447f8ad7187aa956eaf
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-06-14 07:40:59 -0700

    new

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,11 +1,13 @@
 #include "main.h"
 #include "string.h"
 /**
- * strlen - displays the lengh of a string
+ * _strlen - displays the lengh of a string
  * @s: the string to compute length
  * Return: integer
  */
 int _strlen(char *s)
 {
-return (*s);
+int length = strlen(char *s);
+
+return (length);
 }
