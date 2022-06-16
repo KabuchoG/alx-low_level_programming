3032c5ad5dde5a68ae4e92935759526c38aa456b
KabuchoG <105174643+KabuchoG@users.noreply.github.com>
2022-06-16 20:41:17 +0100

    concatenate strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,15 +1,27 @@
 #include "main.h"
-#include <string.h>
 /**
- * strncat - appenda a string from source to destination but with a limit to the size of the destination
- * @dest: the updated string
- * @src: the string to append
- * Return:va pointer to the appended array string
+ * _strncat - concatenates two strings,
+ * @dest: destination.
+ * @src: source.
+ * @n: amount of bytes used from src.
+ * Return: the pointer to dest.
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	n = (sizeof(dest) - strlen(dest) - 1);
-	strncat(dest, src, n);
+	int cycle = 0, cycle2 = 0;
+
+	while (*(dest + cycle) != '\0')
+	{
+		cycle++;
+	}
+
+	while (cycle2 < n)
+	{
+		*(dest + cycle) = *(src + cycle2);
+		if (*(src + cycle2) == '\0')
+			break;
+		cycle++;
+		cycle2++;
+	}
 	return (dest);
 }
-
