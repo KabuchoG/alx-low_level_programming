a3a264aa8837345f3871d06f9432016e7e01a2aa
KabuchoG <105174643+KabuchoG@users.noreply.github.com>
2022-06-16 21:32:04 +0100

    final

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,14 +9,15 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0, elem = 0;
+	int i = 0, op = 0;
 
-while (elem == 0)
+	while (op == 0)
 	{
 		if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
 			break;
-		elem = *(s1 + i) - *(s2 + i);
+		op = *(s1 + i) - *(s2 + i);
 		i++;
 	}
-	return (elem);
+
+	return (op);
 }
