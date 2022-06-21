a7239b1593037066bc597cac681c004b242ebd0b
KabuchoG <105174643+KabuchoG@users.noreply.github.com>
2022-06-21 19:04:31 +0100

    final commit

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,24 +1,26 @@
-#include "main.h"
-
+#include"main.h"
 /**
- * _strstr - find the first occurence of a substring 
- * @haystack: the main string
- * @needle: pointer to the substring
- * Return:pointer to the begining of the located substring
- */
+ * _strstr - locating a substring
+ * @needle: occurance to find
+ * @haystack: string to search
+ * Return: pointer
+*/
 char *_strstr(char *haystack, char *needle)
 {
-int i;
 
-while (*haystack != '\0')
-{
-while (*needle != '\0')
-{
-if (*haystack == *needle)
-	return (haystack);
-	needle++
-}
-	haystack++;
-}
+	while (*haystack)
+	{
+	char *src = haystack;
+	char *sub = needle;
+
+	while (*haystack && *sub && *haystack == *sub)
+	{
+		haystack++;
+		sub++;
+	}
+	if (!*sub)
+	return (src);
+	haystack = src + 1;
+	}
 return (0);
 }
