2084f351c380b1fda0b3e6eba9b1df968ee16564
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-06-21 04:52:03 -0700

    new

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strchr - locates a character in a string.
+ * @s: the string.
+ * @c: the character
+ * Return: s if c is found and Null if not
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; i != '\0'; i++)
+	{
+	if (s[i] == c)
+	{
+	return (s);
+	}
+	}
+	return (0);
+}
