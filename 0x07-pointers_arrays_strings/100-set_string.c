561eb0e9f589fd669dfee24508a43c2624acb74f
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-06-21 08:49:42 -0700

    new commit

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,11 +1,14 @@
 #include "main.h"
 /**
- *
+ * set_string -set the value of a pointer to a char.
+ * @s: pointer to a pointer
+ * @to: pointer to value.
+ * Return: void
  */
 void set_string(char **s, char *to)
 {
 	char c;
 
-	to = &c;
+	to = c;
 	s = &to;
 }
