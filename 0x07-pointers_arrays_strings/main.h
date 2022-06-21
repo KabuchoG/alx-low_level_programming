e5677dc756097a5bc77164f1dd7c5b83e1d01f7f
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-06-21 10:13:14 -0700

    new commit

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -6,5 +6,6 @@ char *_strchr(char *s, char c);
 char *_strpbrk(char *s, char *accept);
 unsigned int _strspn(char *s, char *accept);
 void set_string(char **s, char *to);
+char *_strstr(char *haystack, char *needle);
 
 #endif
