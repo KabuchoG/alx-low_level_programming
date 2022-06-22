abddd0a6fed1b0889a550e86fd35e0e6dfaa432d
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-06-22 12:26:33 -0700

    new commit

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * factorial - prints the factorial of a number
+ * @n: incoming number
+ * Return: integer
+ */
+int factorial(int n);
+{
+if (n < 0)
+{
+	return (-1);
+}
+if (n == 0)
+{
+	return (1);
+}
+else
+{
+return (n * factorial(n-1));
+}
+return (0);
+}
