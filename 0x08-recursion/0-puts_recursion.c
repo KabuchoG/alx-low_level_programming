c70255385112efebbcd2d0eec7e47845dda06f94
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-06-22 09:36:41 -0700

    new comit

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a string
+ * @s: the pointer to anincoming variable containing a string
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+_putchar(*s);
+}
