9e7358c24b6e91709f07ff54db3eaaf3bf14520d
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-06-22 13:04:13 -0700

    new commit

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * is_prime_number - finds a prime number
+ * @: the integer variable
+ * Return: integer
+ */
+int is_prime_number(int n)
+{
+if ((n % 10) != 0)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+return (0);
+}
