21947f7e603bd0f1348c9a19f03cce3162365330
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-06-22 12:32:31 -0700

    new commit

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -4,7 +4,7 @@
  * @n: incoming number
  * Return: integer
  */
-int factorial(int n);
+int factorial(int n)
 {
 if (n < 0)
 {
@@ -16,7 +16,7 @@ if (n == 0)
 }
 else
 {
-return (n * factorial(n-1));
+return (n * factorial(n - 1));
 }
 return (0);
 }
