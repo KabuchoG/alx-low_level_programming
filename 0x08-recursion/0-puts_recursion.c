444511da247c9096d9298e0b36d422362a21a864
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-06-22 09:58:25 -0700

    new commit

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,11 +6,10 @@
  */
 void _puts_recursion(char *s)
 {
-	int i;
-
-	for (i = 0; i != '\0'; i++)
+	if (*s != '\0')
 	{
-_putchar(s[i]);
+_putchar(*s);
+_putchar(_put_srecursion(*s + 1));
 	}
 _putchar('\n');
 }
