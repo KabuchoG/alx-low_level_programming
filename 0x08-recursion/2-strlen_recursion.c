fbbfcd87ac18492dbc32a5968da39ac8eacaf6c5
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-06-22 11:50:21 -0700

    new commit

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,9 +6,12 @@
  */
 int _strlen_recursion(char *s)
 {
-if (*s != '\0')
+int i;
+
+if (s[i] != '\0')
 {
-_strlen_recursion(s + 1);
-return (s);
+_strlen_recursion(s[i] + 1);
+return (i);
 }
+return (0);
 }
