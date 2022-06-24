b57c82107c03eb64f5997260a66ad42f25567238
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-06-24 03:23:00 -0700

    new commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * main - prints the name of a program
  * @argc: the number of arguments
@@ -8,5 +9,7 @@
 int main(int argc, char *argv[])
 {
 _putchar(argv[0]);
+printf("\n");
+(void)argc;
 return (0);
 }
