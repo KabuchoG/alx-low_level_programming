1680df49907756cb229c8a651b65083ef3bbdeb1
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-06-24 03:35:24 -0700

    new commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - perints the number of arguments passed into it
+ * @argc: number of arguments
+ * @argv: the argumrnts 
+ * Return: integer
+ */
+int main(int argc, char *argv[])
+{
+printf("%d\n", argc);
+(void)argv;
+return (0);
+}
