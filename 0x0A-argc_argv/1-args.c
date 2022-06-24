69ebfa2d4067e85cd09087e96ff5087154fff082
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-06-24 03:48:24 -0700

    new commit

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,7 +5,7 @@
  * @argv: the argumrnts.
  * Return: integer
  */
-int main(int argc, char *argv[] _attribute_((unused)))
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 printf("%d\n", argc);
 return (0);
