d529abdab3b38836aada18fc4ab88cc60062cd5d
KabuchoG <105174643+KabuchoG@users.noreply.github.com>
2022-06-24 15:38:04 +0100

    new commit

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,32 +1,28 @@
 #include <stdio.h>
-#include <ctype.h>
 #include <stdlib.h>
+#include <ctype.h>
 /**
- * main - sums up two numbers
- * @argc: number of arguments passed
- * @argv: the string arrays
- * Return: integer
+ * main - adds positive numbers.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
  */
 int main(int argc, char *argv[])
 {
-	int i, sum = 0;
+	int i, j, add = 0;
 
-	if (argc == 1)
-	{
-	printf("0\n");
-	}
 	for (i = 1; i < argc; i++)
 	{
-	if (isdigit(argv[i]))
-	{
-		sum += atoi(argv[i]);
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		add += atoi(argv[i]);
 	}
-	else
-	{
-		printf("Error");
-		return (1);
-	}
-	}
-	printf("%d\n", sum);
+	printf("%d\n", add);
 	return (0);
 }
