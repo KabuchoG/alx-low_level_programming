2d31f9be95803b1c435628ca84e2c180c7d106d0
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-06-28 06:53:28 -0700

    new commit

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array - creates an array of chars
+ * @size: size of array
+ * @c: the string of chars
+ */
+char *create_array(unsigned int size, char c)
+{
+	c [0] = 'l';
+	size = sizeof(c[0]);
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		return (NULL);
+	}
+	return (c);
+}
