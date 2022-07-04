066e1f8495e37d65e7bfb804ae8ec325eebd0ddd
KabuchoG <105174643+KabuchoG@users.noreply.github.com>
2022-07-04 22:41:12 +0100

    new commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,17 +1,25 @@
-#ifndef DOG
-#define DOG
+#ifndef DOG_H
+#define DOG_H
+
 /**
- * struct dog - just a sample structure
+ * struct dog - Struct dog
  * @name: name
  * @age: age
- * @owner: owner
- * Description: sample
+ * @owner:  owner
+ * Description: This struct is for dog
  */
+
 struct dog
-	{
-	char* name;
+{
+	char *name;
 	float age;
 	char *owner;
-	};
+};
+
 typedef struct dog dog_t;
-#endif
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif /*DOG_H*/
