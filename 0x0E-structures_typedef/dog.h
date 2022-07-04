679eda2afaa41ede6d65eca334e4ca3b99c833e8
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-07-04 12:41:59 -0700

    new commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+/**
+ * struct dog - just a sample structure
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Description: sample
+ */
+struct dog
+	{
+	char* name;
+	float age;
+	char *owner;
+	}
