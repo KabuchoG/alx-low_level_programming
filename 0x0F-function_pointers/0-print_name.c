2a198aebed8f12902a25ea059a9ac27daeb2e51b
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-07-06 10:22:42 -0700

    new commit

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,17 +1,12 @@
 #include "function_pointers.h"
 /**
  * print_name - prints a name
- * @name: araay of strings
+ * @name: array of strings
  * @f: pointer to a funtion
  * Return: void
  */
 void print_name(char *name, void (*f)(char *))
 {
-int i;
-
-for(i = 0; i != '\0'; i++)
-{
-_putchar(name[i]);
-}
-(void)f();
+if (name && f)
+	f(name);
 }
