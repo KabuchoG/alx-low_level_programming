6ded88d1981be02863efb2d40ac6d50fcee94a56
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-07-14 16:15:35 -0700

    new commit

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,21 @@
+#include <string.h>
+#include "lists.h"
+/**
+ * add_node - adds a node at the begining of a list
+ * @head: a pointer to a pointer to the adress of the first element of the list(first node)
+ * @str: a string to be added in the new node
+ * Return: a value of type list_t- address of the new node
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+list_t *new = malloc(sizeof(list_t));
+
+if (new == NULL)
+	return (NULL);
+new->str = strdup(str);
+new->len = strlen(new->str);
+
+new->next = *head;
+*head = new;
+return (new);
+}
