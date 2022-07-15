281c0e218650a45269fadbe0c72c53c076fe9454
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-07-15 01:44:41 -0700

    new commit

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,6 +11,7 @@ list_t *add_node(list_t **head, const char *str)
 {
 list_t *new = malloc(sizeof(list_t));
 if (new == NULL)
+free(new);
 return (NULL);
 new->str = strdup(str);
 new->len = strlen(new->str);
