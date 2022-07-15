eca208db60e5b1c3c807675693fe374cbfa8f352
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-07-15 03:42:54 -0700

    new commit

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * free_list - frees an entire list
+ * @head: pointer to the firsst element of a list
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+list_t *ptr = head;
+while (ptr != NULL)
+{
+	ptr = ptr->next;
+	free(head);
+	head = ptr;
+}
+}
