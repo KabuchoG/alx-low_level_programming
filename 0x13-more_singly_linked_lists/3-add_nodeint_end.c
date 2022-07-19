05cd7ddd4d28dde31a281d728fc6bf23cdc167c0
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-07-19 01:26:08 -0700

    new commit

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -10,11 +10,8 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 listint_t *ptr = *head;
 listint_t *new = malloc(sizeof(listint_t));
 
-if (new = NULL)
-{
-free(new);
+if (new == NULL)
 return (NULL);
-}
 new->n = n;
 new->next = NULL;
 while (ptr->next != NULL)
