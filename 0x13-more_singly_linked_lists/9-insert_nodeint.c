6bcbfa8bad721055a5b652890ac463e08c14b2ea
KabuchoG <105174643+KabuchoG@users.noreply.github.com>
2022-07-20 19:12:00 +0100

    new commit

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,39 +1,45 @@
 #include "lists.h"
+#include <stdlib.h>
+
 /**
- * insert_nodeint_at_index - inserts a new node at a given position
- * @head: pointer to the first node of the list
- * @idx: index position
- * @n: value of the new node
- * Return: adress of the new node
+ * insert_nodeint_at_index -  inserts a new node .
+ * @head: pointer to the first element of the list
+ * @idx: index of the node
+ * @n: value of the node
+ *
+ * Return: pointer to the allocated node .
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-listint_t *ptr = *head;
-listint_t *new = malloc(sizeof(listint_t));
-listint_t *pos = ptr;
-unsigned int i;
+	unsigned int i;
+	listint_t *temp, *node;
 
-if (new == NULL)
-{
-	free(new);
-	return (NULL);
+	if (head == NULL)
+		return (NULL);
+	if (idx)
+	{
+		temp = *head;
+		for (i = 0; i < idx - 1 && temp ; i++)
+		{
+			temp = temp->next;
+		}
+		if (!temp)
+			return (NULL);
+	}
+	node = (listint_t *)malloc(sizeof(listint_t));
+	if (node == NULL)
+		return (NULL);
+	node->n = n;
+	if (idx == 0)
+	{
+		node->next = *head;
+		*head = node;
+		return (node);
+	}
+	node->next = temp->next;
+	temp->next = node;
+	return (node);
 }
-new->n = n;
-if (*head == NULL)
-{
-	free(new);
-	return (NULL);
-}
-for (i = 0; i < idx; i++)
-{
-if (idx == 0)
-{
-	new->next = *head;
-	*head = new;
-	return (new);
-}
-pos = ptr;
-ptr = ptr->next;
 if (ptr == NULL)
 {
 	free(new);
