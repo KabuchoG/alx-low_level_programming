e2238c47f85cd8308919071c6a5d3342acacc2ca
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-07-20 04:41:30 -0700

    new commit

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,19 +1,18 @@
 #include "lists.h"
 /**
- * sum_listint: sums the dara in all the nodes of the list
+ * sum_listint - sums the dara in all the nodes of the list
  * @head: pointer to the first node of the list
  * Return: the sum of the node data
  */
 int sum_listint(listint_t *head)
 {
-listint_t *ptr = head;
 int sum = 0;
+listint_t *ptr = head;
 
 while (ptr != NULL)
 {
 	sum += ptr->n;
 	ptr = ptr->next;
 }
-free(ptr);
 return (sum);
 }
