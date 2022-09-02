85b41ae0e10561de0a64064b9a991d8617dbde8b
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-09-02 06:20:01 -0700

    new commit

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * add_dnodeint - add a node at the begining
+ * @head: pointer to pointer to the first element
+ * @n: the value
+ * Return: The return value pointer to the address of the new node
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new = malloc(sizeof(dlistint_t));
+
+	new->prev = NULL;
+	new->n = n;
+	new->next = NULL;
+	if (*head == NULL)
+		return (new);
+	new->next = *head;
+	(*head)->prev = new;
+	*head = new;
+	return (*head);
+}
