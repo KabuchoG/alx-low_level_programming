6a4cd0e33b7f0bc14ca75839ab1805ea169e10b3
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-09-02 11:51:27 -0700

    new commit

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * sum_dlistint - returns the sum of all the data n in the doubly linked list
+ * @head: pointer to the first element of the list
+ * Return: sum of the data
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *h = head;
+	int sum = 0;
+
+	if (head == NULL)
+		return (sum);
+	while (h != NULL)
+	{
+	sum += + h->n;
+	h = h->next;
+	}
+	return (sum);
+}
