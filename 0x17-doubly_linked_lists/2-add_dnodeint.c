7506fffb1c2a1a5f6aa3889bc7a28d02c12ecc0d
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-09-02 07:17:40 -0700

    new commit

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -8,7 +8,8 @@
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new = malloc(sizeof(dlistint_t));
-
+	dlistint_t *h;
+	h = *head;
 	if (new == NULL)
 		return (NULL);
 	new->prev = NULL;
@@ -20,9 +21,9 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	}
 	else
 	{
-	new->next = *head;
-	(*head)->prev = new;
+	new->next = h;
+	h->prev = new;
 	*head = new;
-	return (*head);
+	return (new);
 	}
 }
