0b1690308eeb45e2a7878d63cabdb88e18030b38
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-09-02 05:30:08 -0700

    new commit

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -13,8 +13,6 @@ size_t print_dlistint(const dlistint_t *h)
 	const dlistint_t *ptr = h;
 	if (ptr == NULL)
 		return (i);
-	while (ptr->prev != NULL)
-		ptr = ptr->prev;
 	while (ptr != NULL)
 	{
 		printf("%d", ptr->n);
