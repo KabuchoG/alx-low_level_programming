98b8b57b33c07556e29b4c5ddb8d2307bfd105b1
KabuchoG <105174643+KabuchoG@users.noreply.github.com>
2022-09-02 22:25:45 +0100

    new commit

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,41 +1,54 @@
 #include "lists.h"
+
 /**
- * insert_dnodeint_at_index - inserts a node at an index
- * @h: pointer to pointer to the first element of a list
- * @idx: positional index where to insert
- * @n: the data
- * Return: return the adress of the new node
+ * insert_dnodeint_at_index - inserts a new node at
+ * a given position
+ *
+ * @h: head of the list
+ * @idx: index of the new node
+ * @n: value of the new node
+ * Return: the address of the new node, or NULL if it failed
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *ptr = *h;
-	unsigned int i = 0;
-	dlistint_t *tmp;
-	dlistint_t *new = malloc(sizeof(dlistint_t));
+	dlistint_t *new;
+	dlistint_t *head;
+	unsigned int i;
 
-	tmp = ptr;
-	if (new == NULL)
-		return (NULL);
-	new->n = n;
+	new = NULL;
 	if (idx == 0)
-		add_dnodeint(h, n);
-	while (ptr != NULL )
-{
-	if (i == idx)
+		new = add_dnodeint(h, n);
+	else
 	{
-	tmp->next = new;
-	ptr->prev = new;
-	new->prev = tmp;
-	new->next = ptr;
-	return (new);
+		head = *h;
+		i = 1;
+		if (head != NULL)
+			while (head->prev != NULL)
+				head = head->prev;
+		while (head != NULL)
+		{
+			if (i == idx)
+			{
+				if (head->next == NULL)
+					new = add_dnodeint_end(h, n);
+				else
+				{
+					new = malloc(sizeof(dlistint_t));
+					if (new != NULL)
+					{
+						new->n = n;
+						new->next = head->next;
+						new->prev = head;
+						head->next->prev = new;
+						head->next = new;
+					}
+				}
+				break;
+			}
+			head = head->next;
+			i++;
+		}
 	}
-	ptr = ptr->next;
-	tmp = ptr->prev;
-	i++;
-}
-	if (i == idx)
-{
-		add_dnodeint_end(h, n);
-}
-	return (NULL);
+
+	return (new);
 }
