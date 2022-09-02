f7c3063c0fb7a640a57fc9bc0bd51adfa85831d5
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-09-02 13:51:57 -0700

    new commit -m

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+/**
+ * insert_dnodeint_at_index - inserts a node at an index
+ * @h: pointer to pointer to the first element of a list
+ * @idx: positional index where to insert
+ * @n: the data
+ * Return: return the adress of the new node
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *ptr = *h;
+	unsigned int i = 0;
+	dlistint_t *tmp;
+	dlistint_t *new = malloc(sizeof(dlistint_t));
+
+	tmp = ptr;
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	if (*h == NULL)
+		return (NULL);
+	if (idx == 0)
+		add_dnodeint(h, n);
+	while (ptr != NULL )
+{
+	if (i == idx)
+	{
+	tmp->next = new;
+	ptr->prev = new;
+	new->prev = tmp;
+	new->next = ptr;
+	return (new);
+	}
+	ptr = ptr->next;
+	tmp = ptr->prev;
+	i++;
+}
+	if (i == idx)
+{
+		add_dnodeint_end(h, n);
+}
+	return (NULL);
+}
